cfc2ecd4c701000f06c2d2c0b94674193e866ca6
Stars1104 <cooljob.love1104@gmail.com>
2025-03-28 15:06:19 -0700

    created zoomerang desktop frontend

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Homepage = () => {
+    return (
+        <div className="w-full flex justify-center items-center min-h-screen bg-red-500">This is Homepage</div>
+    )
+}
+
+export default Homepage;
---
+++ src/router/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "../pages/Homepage";
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
